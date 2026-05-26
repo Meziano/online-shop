0015efdfe22d8d4d792b02b2399249eb8a70bcda
Meziane Kettou <me.kettou@gmail.com>
2026-05-26 13:57:11 +0200

    fix: correct firstName to givenName, lastName to familyName

--- services/customer-service/src/main/java/de/meziane/services/customer/api/dto/CustomerResponse.java
+++ services/customer-service/src/main/java/de/meziane/services/customer/api/dto/CustomerResponse.java
@@ -1,14 +1,13 @@
 package de.meziane.services.customer.api.dto;
 
 import de.meziane.common.model.CustomerGender;
-import de.meziane.services.customer.persistence.entity.CustomerContact;
 
 import java.time.LocalDate;
 import java.util.List;
 
 public record CustomerResponse(Long id,
-                               String firstName,
-                               String lastName,
+                               String givenName,
+                               String familyName,
                                LocalDate dob,
                                CustomerGender gender,
                                List<CustomerContactResponse> contacts) {
