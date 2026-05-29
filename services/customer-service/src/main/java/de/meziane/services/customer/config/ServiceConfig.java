65c3e34209ec4eb59810c6affeff7cf895135eed
Meziane Kettou <me.kettou@gmail.com>
2026-05-29 20:19:41 +0200

    chore: add OpenAPI config and enable JPA auditing in services

---
+++ services/customer-service/src/main/java/de/meziane/services/customer/config/ServiceConfig.java
@@ -0,0 +1,24 @@
+package de.meziane.services.customer.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@Configuration
+public class ServiceConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Customer Service API")
+                        .version("v1")
+                        .contact(new Contact()
+                        .name("Meziane")
+                        .email("me.k***ou@gmail.com")));
+    }
+}
--- services/product-catalog-service/src/main/java/de/meziane/services/product/catalog/config/JpaAuditingConfig.java
+++
@@ -1,9 +0,0 @@
-package de.meziane.services.product.catalog.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
-
-@EnableJpaAuditing
-@Configuration
-public class JpaAuditingConfig {
-}
